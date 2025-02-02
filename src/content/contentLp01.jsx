518c10360b2de5c32485199859d0801bba4df4da
Edison Matos <edison.matos@live.com>
2025-02-02 12:06:36 -0300

    Initial commit

---
+++ src/content/contentLp01.jsx
@@ -0,0 +1,344 @@
+import bgImg from "../assets/imgs/hero/bgHeroLp01.webp";
+import logo from "../assets/imgs/logo/logoLp.png";
+import { infos } from "./content";
+
+const currentYear = new Date().getFullYear();
+
+const contentLp01 = {
+  infos: {
+    title:
+      "Finance - Soluções Financeiras | Recuperação de Crédito e Empréstimos para todo Brasil",
+    description:
+      "Recupere seu crédito e consiga empréstimos sem burocracia. Nossa equipe especializada oferece soluções rápidas e seguras para você ter o que é seu de volta.",
+    keywords:
+      "Recuperação de crédito, Empréstimos sem burocracia, Recuperação de crédito fácil, Empréstimos rápidos, Empréstimos para negativado, Crédito pessoal, Recuperação de crédito para todo Brasil, Empréstimos com garantia, Soluções financeiras, Recuperação de crédito sem comprovação de renda, Empréstimos rápidos para todo Brasil, Crédito sem análise de CPF, Empréstimos sem juros, Recuperação de crédito para autônomos, Recuperação de crédito para aposentados, Empréstimos para pequenas empresas, Consultoria financeira, Empréstimos para negativados para todo Brasil, Crédito fácil, Soluções financeiras rápidas.",
+  },
+  hero: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    textArea: {
+      title: (
+        <h1>
+          Recupere <span className="text-primary"> crédito </span> e tenha
+          acesso ao
+          <span className="text-primary"> empréstimo </span> que você merece!
+        </h1>
+      ),
+      subtitle:
+        "Com mais de 10 anos de experiência, somos especialistas em recuperar seu crédito e liberar empréstimos sem burocracia. Simples, rápido e sem complicação!",
+      buttonLabel: "Quero falar com um especialista",
+      ctaButtonAriaLabel:
+        "Botão para chamada de ação para contato pelo whatsapp",
+    },
+  },
+  features: {
+    sectionHeader: {
+      miniTag: "Quem pode se beneficiar?",
+      title:
+        "Confira se você ou alguém da sua família pode se beneficiar da nossa ajuda financeira.",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hand-coins"
+          >
+            <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
+            <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
+            <path d="m2 16 6 6" />
+            <circle cx="16" cy="9" r="2.9" />
+            <circle cx="6" cy="5" r="3" />
+          </svg>
+        ),
+        title: "Aposentados e Pensionistas",
+        description:
+          "Aposentados, pensionistas e militares têm isenção de Imposto de Renda, o que facilita a recuperação de crédito e empréstimos com condições especiais.",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-ban"
+          >
+            <circle cx="12" cy="12" r="10" />
+            <path d="m4.9 4.9 14.2 14.2" />
+          </svg>
+        ),
+        title: "Pessoas com Nome Sujo",
+        description:
+          "Mesmo com o nome negativado, é possível recuperar crédito após regularizar as pendências e negociar dívidas.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-briefcase-business"
+          >
+            <path d="M12 12h.01" />
+            <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+            <path d="M22 13a18.15 18.15 0 0 1-20 0" />
+            <rect width="20" height="14" x="2" y="6" rx="2" />
+          </svg>
+        ),
+        title: "Autônomos e Empreendedores",
+        description:
+          "Autônomos e donos de pequenas empresas também têm acesso a empréstimos e facilidades na recuperação de crédito.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-house"
+          >
+            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+            <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+          </svg>
+        ),
+        title: "Pessoas com Garantias",
+        description:
+          "Quem tem garantia, como imóvel ou veículo, pode conseguir empréstimos com taxas de juros mais baixas.",
+      },
+    },
+  },
+  about: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    sectionHeader: {
+      miniTag: "TRADIÇÃO EM RECUPERAÇÃO E EMPRÉSTIMOS",
+      title: "Especialistas em Recuperação de Crédito e Empréstimos",
+    },
+    paragraph: (
+      <p>
+        Com mais de uma década de experiência, nossa equipe é especializada em
+        ajudar pessoas a recuperar seu crédito e acessar empréstimos com
+        condições vantajosas. Entendemos que cada situação é única, por isso
+        realizamos uma análise detalhada e oferecemos um serviço completo, desde
+        a regularização de pendências até a liberação do crédito. <br />
+        <br /> Conte conosco para garantir a solução financeira que você
+        precisa, com rapidez e sem burocracia.
+      </p>
+    ),
+    buttonLabel: "Quero falar com um especialista",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  whyUs: {
+    sectionHeader: {
+      miniTag: "A MELHOR ESCOLHA",
+      title: "Porque escolher a Finance?",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-network"
+          >
+            <rect x="16" y="16" width="6" height="6" rx="1" />
+            <rect x="2" y="16" width="6" height="6" rx="1" />
+            <rect x="9" y="2" width="6" height="6" rx="1" />
+            <path d="M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3" />
+            <path d="M12 12V8" />
+          </svg>
+        ),
+        title: "Equipe Especializada",
+        description:
+          "Profissionais capacitados para resolver toda a parte burocrática e técnica para você.",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-piggy-bank"
+          >
+            <path d="M19 5c-1.5 0-2.8 1.4-3 2-3.5-1.5-11-.3-11 5 0 1.8 0 3 2 4.5V20h4v-2h3v2h4v-4c1-.5 1.7-1 2-2h2v-4h-2c0-1-.5-1.5-1-2V5z" />
+            <path d="M2 9v1c0 1.1.9 2 2 2h1" />
+            <path d="M16 11h.01" />
+          </svg>
+        ),
+        title: "Economia Garantida",
+        description:
+          "Maximizamos seus direitos para aumentar seus recursos financeiros.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-thumbs-up"
+          >
+            <path d="M7 10v12" />
+            <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z" />
+          </svg>
+        ),
+        title: "Processo Simplificado",
+        description:
+          "Cuidamos de tudo, com transparência e segurança para você acompanhar.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-shield-check"
+          >
+            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
+            <path d="m9 12 2 2 4-4" />
+          </svg>
+        ),
+        title: "Segurança e Confidencialidade",
+        description:
+          "Protegemos suas informações com total segurança e confidencialidade.",
+      },
+    },
+  },
+  cta: {
+    bgImg: bgImg,
+    sectionHeader: {
+      miniTag: "NÃO PERCA MAIS TEMPO",
+      title: "Recupere sua estabilidade financeira com mais tranquilidade.",
+      subtitle: `Liberte-se da burocracia, foque no que importa e recupere valores pagos indevidamente, evitando gastos desnecessários no futuro.`,
+    },
+    buttonLabel: "Quero falar com um especialista",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  contact: {
+    card1: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-instagram"
+        >
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+          <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+        </svg>
+      ),
+      title: "Instagram",
+      description: `@${infos.instagramProfile}`,
+    },
+    card2: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-mail"
+        >
+          <rect width="20" height="16" x="2" y="4" rx="2" />
+          <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+        </svg>
+      ),
+      title: "Email",
+      description: `${infos.email}@${infos.domain}`,
+    },
+    card3: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          className="bi bi-whatsapp"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+        </svg>
+      ),
+      title: "Whatsapp",
+      description: `(${infos.phone.ddd}) ${infos.phone.firstPart}-${infos.phone.secondPart}`,
+    },
+  },
+  footer: {
+    copyrightLine: `© ${currentYear} ${infos.name}. Todos os direitos reservados.`,
+    disclaimer: `Este site não é um produto Meta Platforms, Inc., Google LLC, tampouco oferece serviços públicos oficiais. ${infos.name} oferece serviços jurídicos privativos de advogado, de acordo com a legislação vigente e o Código de Ética e Disciplina da Ordem dos Advogados do Brasil.`,
+  },
+  links: {
+    instagram: `https://www.instagram.com/${infos.instagramProfile}/`,
+    ctaWhatsapp: `https://wa.me/+55${infos.phone.ddd}${infos.phone.firstPart}${infos.phone.secondPart}?text=${infos.whatsappDefaultMessage}`,
+  },
+};
+
+export default contentLp01;
