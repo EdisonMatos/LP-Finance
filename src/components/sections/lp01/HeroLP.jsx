0458ce063db4283fb475a0c69f673a54003a2694
Edison Matos <edison.matos@live.com>
2025-02-03 15:07:21 -0300

    feat: Atualiza opacidade

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -14,7 +14,7 @@ export default function HeroLP() {
     >
       <SectionArea
         paddingTopAndBottom={false}
-        className="bg-black bg-opacity-70"
+        className="bg-black bg-opacity-80"
       >
         <SectionWrapper className="">
           <div className="flex w-full ">
